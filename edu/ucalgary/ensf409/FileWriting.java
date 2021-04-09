6ea039c0a50ebbf11c1e11774b822fe5265597dd
Braden Foley <BFman2012@gmail.com>
2021-04-09 14:31:34 -0600

    Removed main from program and removed a unused getter method.

--- edu/ucalgary/ensf409/FileWriting.java
+++ edu/ucalgary/ensf409/FileWriting.java
@@ -20,8 +20,8 @@ public class FileWriting {
     private String[] ID;
     private String filePath;
     private int totalPrice;
-	private String typeWritten; //used for testing to see if correct type of 
-								//message is written (manufacturers or items)
+    private String typeWritten; // used for testing to see if correct type of
+                                // message is written (manufacturers or items)
 
     /**
      * Default constructor
@@ -64,33 +64,34 @@ public class FileWriting {
             output.write("Faculty Name: \nContact: \nDate: \n\n"); // a fill in
             if (totalPrice > 0) {
                 output.write("Items Requested: " + type + ", " + quantity + "\n\n"); // writing the original request
-				System.out.println("\nItems Requested: " + type + ", " + quantity + "\n\n"); // writing the original request
+                System.out.println("\nItems Requested: " + type + ", " + quantity + "\n\n"); // writing the original
+                                                                                             // request
                 output.write("Items Ordered\n");
-				
-				typeWritten = "Items";
-				
-				System.out.println("Items Required\n");
+
+                typeWritten = "Items";
+
+                System.out.println("Items Required\n");
                 for (int i = 0; i < ID.length; i++) { // outputting the ID for the inventory that is used
                     output.write("ID: " + ID[i] + "\n");
-					System.out.println("ID: " + ID[i] + "\n");
+                    System.out.println("ID: " + ID[i] + "\n");
                 }
                 output.write("\nTotal Price: $" + totalPrice); // writing the totatl price
-				System.out.println("Total Price: $" + totalPrice); 
-				System.out.println("\nPlease see OrderSummary.txt to complete your order form");
+                System.out.println("Total Price: $" + totalPrice);
+                System.out.println("\nPlease see OrderSummary.txt to complete your order form");
             } else if (totalPrice == -1) { // in this case we know we couldn't meet the request requirements
                 output.write(
                         "We cannot fulfill your order with the provided requirements based on our current inventory.");
                 output.write(" We suggest you try contacting the following manufactures: \n");
-				System.out.println(
+                System.out.println(
                         "\nWe cannot fulfill your order with the provided requirements based on our current inventory.");
                 System.out.println("We suggest you try contacting the following manufactures: \n");
                 String[] manufactures = idToManufactures(ID);
-				typeWritten = "Manufacturers";
+                typeWritten = "Manufacturers";
                 for (int i = 0; i < manufactures.length; i++) {
                     output.write(manufactures[i] + "\n");
-					System.out.println(manufactures[i] + "\n");
+                    System.out.println(manufactures[i] + "\n");
                 }
-				System.out.println("\nPlease see OrderSummary.txt to complete your order form");
+                System.out.println("\nPlease see OrderSummary.txt to complete your order form");
             } else {
                 System.err.println("Somthing went wrong in the totalPrice calculation!");
                 System.exit(1); // there was an error computing the total price
@@ -134,14 +135,4 @@ public class FileWriting {
         return temp;
     }
 
-	public String getTypeWritten()
-	{
-		return this.typeWritten;
-	}
-    public static void main(String[] args) {
-        String id[] = { "001", "004", "005" };
-        FileWriting writer = new FileWriting("test", 12, id, "C:\\ENSF409\\Final_Project\\edu\\ucalgary\\ensf409\\",
-                -2);
-        writer.writeFile();
-    }
 }
